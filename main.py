5ca2f1776f47adc9aac58b9cf9ee7d6c6adffee7
Vitaliy <bering856@gmail.com>
2025-02-18 08:46:14 +0200

    remove unused audio and graphic assets; refactor main game logic for improved performance

--- main.py
+++ main.py
@@ -1,114 +1,136 @@
-import pygame
-import sys
+import pygame, sys
+from random import choice
 
-MEDIUM_AQUAMARINE_COLOR = (111, 196, 169)
+def ball_animation():
+    global ball_speed_x, ball_speed_y, player_score, opponent_score, score_time
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
 
-def display_score():
-    current_time = int(pygame.time.get_ticks() / 1000) - start_time
-    score_surf = test_font.render(f"{current_time}", False, (64, 64, 64))
-    score_rect = score_surf.get_rect(center = (400, 50))
-    screen.blit(score_surf, score_rect)
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
 
-    return current_time
+    if ball.left <= 0:
+        player_score += 1
+        score_time = pygame.time.get_ticks()
+
+    if ball.right >= screen_width:
+        opponent_score += 1
+        score_time = pygame.time.get_ticks()
+        
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+def player_animation():
+    player.y += player_speed 
+
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+
+def opponent_ai():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
+
+def ball_restart():
+    global ball_speed_y, ball_speed_x, score_time
+
+    current_time = pygame.time.get_ticks()
+    ball.center = (screen_width / 2, screen_height / 2)
+
+    if (current_time - score_time) < 700:
+        number_three = game_font.render("3", False, light_grey)
+        screen.blit(number_three, (screen_width / 2 - 10, screen_height / 2 + 20))
+
+    if 700 < (current_time - score_time) < 1400:
+        number_two = game_font.render("2", False, light_grey)
+        screen.blit(number_two, (screen_width / 2 - 10, screen_height / 2 + 20))
+
+    if  1400 < (current_time - score_time) < 2100:
+        number_one = game_font.render("1", False, light_grey)
+        screen.blit(number_one, (screen_width / 2 - 10, screen_height / 2 + 20))
+    
+    if (current_time - score_time) > 2100:
+        score_time = None
+        ball_speed_x = 7 * choice((1, -1))
+        ball_speed_y = 7 * choice((1, -1))
+            
+    else:
+        ball_speed_x, ball_speed_y = 0, 0
+
+   
+
 
 pygame.init()
-screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('My game')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
-game_active = False
-start_time = 0
-score = 0
 
-sky_surf = pygame.image.load('./graphics/Sky.png').convert_alpha()
-ground_surf = pygame.image.load('./graphics/ground.png').convert_alpha()
+screen_width = 1280
+screen_height = 960
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-score_surf = test_font.render('My game', False, (60, 60, 60))
-score_rect = score_surf.get_rect(center=(400, 50))
+bg_color = pygame.Color("grey12")
+light_grey = (200, 200, 200)
 
-#Obstacles
-snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surf.get_rect(midbottom=(800, 232))
+pygame.display.set_caption("Pong")
 
-obstacle_list = []
+ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
+opponent = pygame.Rect(10, screen_height / 2 - 70, 10, 140)
 
-player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
-player_rect = player_surf.get_rect(midbottom=(50, 232))
+ball_speed_x = 7 * choice((1, -1))
+ball_speed_y = 7 * choice((1, -1))
+player_speed = 0
+opponent_speed = 7
 
-player_gravity = 0
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf", 32)
 
-#Intro screen
-player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
-player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
-player_stand_rect = player_stand.get_rect(center=(400, 200))
-
-game_name = test_font.render("Pixel Runner", False, MEDIUM_AQUAMARINE_COLOR)
-game_name_rect = game_name.get_rect(center=(400, 80))
-
-game_message = test_font.render('Press space to run', False, MEDIUM_AQUAMARINE_COLOR)
-game_message_rect = game_message.get_rect(center=(400, 330))
-
-#Timer
-obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer, 900)
+score_time = True
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed += 7
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -= 7
+            if event.key == pygame.K_UP:
+                player_speed += 7
+
+    ball_animation()
+    player_animation()
+    opponent_ai()
+
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0), (screen_width / 2, screen_height))
+
+    player_text = game_font.render(f"{player_score}", False, light_grey)
+    screen.blit(player_text, (660, 470))
+    opponent_text = game_font.render(f"{opponent_score}", False, light_grey)
+    screen.blit(opponent_text, (600, 470))
+
+    if score_time:
+        ball_restart() 
         
-        if game_active:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if player_rect.collidepoint(mouse_pos) and player_rect.bottom >= 232:
-                    player_gravity = -20 
-            
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and player_rect.bottom >= 232:
-                        player_gravity = -20
-        else:
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                game_active = True
-                snail_rect.left = 800
-                start_time = int(pygame.time.get_ticks() / 1000)
-        if event.type == obstacle_timer and game_active:
-            obstacle_list.append(snail_surf.get_rect(midbottom=(800, 232)))
-
-    if game_active:
-        screen.blit(sky_surf, (0, 0))
-        screen.blit(ground_surf, (0, 232))
-        # pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        # pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
-        # screen.blit(score_surf, score_rect)
-        score = display_score()
-
-        snail_rect.x -= 4
-        screen.blit(snail_surf, snail_rect)
-        screen.blit(player_surf, player_rect)
-
-        if snail_rect.right <= 0:
-            snail_rect.left = 800
-        
-        player_gravity += 1
-        player_rect.y += player_gravity
-
-        if player_rect.bottom >= 232:
-            player_rect.bottom = 232
-
-        if player_rect.colliderect(snail_rect):
-            game_active = False
-
-    else:
-        screen.fill((94, 129, 162))
-        screen.blit(game_name, game_name_rect)
-        score_message = test_font.render(f'Your score: {score}', False, MEDIUM_AQUAMARINE_COLOR)
-        score_message_rect = score_message.get_rect(center=(400, 330))
-        if score:
-            screen.blit(score_message, score_message_rect)
-        else:
-            screen.blit(game_message, game_message_rect)
-        screen.blit(player_stand, player_stand_rect)
-
-    pygame.display.update()
+    pygame.display.flip()
     clock.tick(60)
+
